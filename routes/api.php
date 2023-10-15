6c9e6a95a7746456689d28fd7c180eb83b86e58f
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-10-15 19:20:03 +0700

    setting up driver model

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DriverController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,9 @@ Route::post('/login/verify',[LoginController::class,'verify']);
 
 
 Route::group(['middleware'=>'auth:sanctum'],function(){
+    Route::get('/driver',[DriverController::class,'show']);
+    Route::post('/driver',[DriverController::class,'update']);
+
     Route::get('/user',function(Request $request){
         return $request->user();
     });
